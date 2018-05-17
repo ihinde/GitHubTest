b788403d873a6973ce5ab953cf2befbe9531433d
Ian Hinde <ian.hinde@papertech.ca>
2018-05-17 14:31:13 -0700

    first edit

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -17,6 +17,8 @@ namespace GitHubTest
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // my first edit
         }
     }
 }
